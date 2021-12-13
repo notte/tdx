1cb185781af1337f19ef5898061e9cf790921e9c
Yoyo <maonome@gmail.com>
2021-12-13 10:24:26 +0800

    create project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="nav"></div>
+  <router-view />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import "./assets/scss/common.scss";
+
+export default defineComponent({});
+</script>
