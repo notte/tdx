941aecb44f7d9cbf8956c002c2bb9cc3bf05fb81
Yoyo <maonome@gmail.com>
2021-12-23 15:17:59 +0800

    modify any

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,6 @@ export default defineComponent({
   name: "App",
   setup() {
     onMounted(() => {
-      // 建立 svg 容器
       const svg = d3
         .select("#nav")
         .append("svg")
@@ -19,12 +18,11 @@ export default defineComponent({
         .attr("height", "100%")
         .attr("style", "border:3px solid #999999")
         .attr("id", "container");
-      // 設定投影方式
-      let projection = d3.geoMercator().center([121, 24]).scale(100);
-      // 產生 path function
-      let path = d3.geoPath().projection(projection) as any;
+      // 121, 24
+      let projection = d3.geoMercator().center([121, 24]).scale(1000);
+      let path = d3.geoPath().projection(projection);
       console.log(path);
-      // 取得 地圖資料
+
       d3.json<Model.IResponseData>("./map.geojson")
         .then((data) => {
           if (data) {
@@ -33,13 +31,14 @@ export default defineComponent({
               .data(data.features)
               .enter()
               .append("path")
-              .attr("d", path)
-              .attr("id", (d, i) => {
-                return "data" + i;
+              .attr("d", path as never)
+              .attr("id", (d) => {
+                return d.properties["COUNTYENG"].split(" ")[0];
               });
           }
         })
         .catch();
+      svg.call(d3.zoom());
     });
 
     return {};
