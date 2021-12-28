12735020e604b335e8907424c4babb0a49f53d4b
Yoyo <maonome@gmail.com>
2021-12-28 18:22:16 +0800

    zoom done

--- src/App.vue
+++ src/App.vue
@@ -13,41 +13,41 @@ export default defineComponent({
     onMounted(() => {
       let width = window.innerWidth,
         height = window.innerHeight;
-      const svg = d3
+      let projection = d3
+        .geoMercator()
+        .scale(10000)
+        .translate([width / 2, height / 2 - 80])
+        .center([121, 24]);
+      let path = d3.geoPath().projection(projection);
+      let svg = d3
         .select("#nav")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("style", "border:3px solid #999999")
         .attr("id", "container");
+      let g = svg.append("g").attr("id", "group");
 
-      let projection = d3
-        .geoMercator()
-        .scale(100)
-        .translate([width / 2, height / 2 - 80])
-        .center([121, 24]);
-      let path = d3.geoPath().projection(projection);
+      d3.json<Model.IResponseData>("./map.geojson").then((data) => {
+        if (data) {
+          g.selectAll("path")
+            .data(data.features)
+            .enter()
+            .append("path")
+            .attr("d", path as never)
+            .attr("id", (d) => {
+              return d.properties["COUNTYENG"].split(" ")[0];
+            });
+        }
+      });
 
-      let zoom = d3.zoom().scaleExtent([1, 30]);
-      console.log(zoom);
-
-      d3.json<Model.IResponseData>("./map.geojson")
-        .then((data) => {
-          if (data) {
-            svg
-              .selectAll("path")
-              .data(data.features)
-              .enter()
-              .append("path")
-              .attr("d", path as never)
-              .attr("id", (d) => {
-                return d.properties["COUNTYENG"].split(" ")[0];
-              })
-              .call(zoom as never);
-          }
-        })
-        .catch();
-      svg.call(d3.zoom());
+      let zoom = d3
+        .zoom()
+        .scaleExtent([1, 3])
+        .on("zoom", function (event) {
+          g.selectAll("path").attr("transform", event.transform);
+        });
+      svg.call(zoom as never);
     });
 
     return {};
